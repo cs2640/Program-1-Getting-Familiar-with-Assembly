fe6c4ee9008eba960b7128878344fe3b0ad3254c
bichidol <125827646+bichidol@users.noreply.github.com>
2023-04-16 12:29:08 -0700

    Update program2task2.asm

--- program2task2.asm
+++ program2task2.asm
@@ -13,34 +13,34 @@ result: .asciiz "'x' to the power 'y' is: "
 
 .text
 main:
-	#Print prompt for user to enter x
-	li $v0, 4
-	la $a0, xNum
-	syscall
+    #Print prompt for user to enter x
+    li $v0, 4
+    la $a0, xNum
+    syscall
 	
-	#Receive user number for x
-	li $v0, 5
-	syscall
-	#store x in $t0
-	move $t0, $a0
+    #Receive user number for x
+    li $v0, 5
+    syscall
+    #store x in $t0
+    move $t0, $a0
 	
-	#Print prompt for user to enter y
-	li $v0, 4
-	la $a0, yNum
-	syscall
+    #Print prompt for user to enter y
+    li $v0, 4
+    la $a0, yNum
+    syscall
 	
-	#Receive user number for y
-	li $v0, 5
-	syscall
-	#store y in $t1
-	move $t1, $a0
+    #Receive user number for y
+    li $v0, 5
+    syscall
+    #store y in $t1
+    move $t1, $a0
     
     #initialize counter
     li $t2, 1
 	
 loop:
-	#Add looping functionality here
-	#loop repeats mult and exits
+    #Add looping functionality here
+    #loop repeats mult and exits
     
     #mult x by itself y times
     mul $t0, $t0, $t0
@@ -61,5 +61,5 @@ loop:
     syscall
 
 exit:
-	li $v0, 10
-	syscall
+    li $v0, 10
+    syscall
